b922863f81aa30416bb3e7099cda6861f1dcb063
ikonovalov <konovalov84@gmail.com>
2014-10-10 00:18:41 +0400

    Add benchmark to drb calls

--- lib/client.rb
+++ lib/client.rb
@@ -1,19 +1,28 @@
 require 'drb'
+require 'benchmark'
+require '../lib/config'
+
+include Benchmark, DRb
+
 DRb.start_service
 shared_object = DRbObject.new(nil, DrbConfig::server_url)
 
 def doRemoteWork(limit, r_object)
-  0..limit.times do
-    r_object.inc(:counter)
-    r_object[:last_access] = Time.now
-  end unless r_object.nil?
+  time = Benchmark.measure do
+    0..limit.times do
+      r_object.inc(:counter)
+      r_object[:last_access] = Time.now
+    end unless r_object.nil?
 
-  puts "Last access time = #{r_object[:last_access]} and counter is #{r_object[:counter]}"
+    puts "Last access time = #{r_object[:last_access]} and counter is #{r_object[:counter]}"
+  end
+  puts CAPTION
+  puts time
 end
 
 begin
-  doRemoteWork 100, shared_object
-rescue DRb::DRbConnError => drb_e
+  doRemoteWork 2000, shared_object
+rescue DRbConnError => drb_e
   puts "Connection failed. #{drb_e.message}"
 end
 

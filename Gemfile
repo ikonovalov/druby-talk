d35c56d8ed723e4a15f1b210e60fcbbc0d3dc839
ikonovalov <konovalov84@gmail.com>
2014-10-11 00:26:45 +0400

    add debugger. Create separated server class.

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source 'https://rubygems.org'
 ruby '2.1.3'
 
 gem 'pry'
+gem 'debase'
 #gem 'spork'
--- lib/server.rb
+++ lib/server.rb
@@ -3,6 +3,21 @@ require 'drb'
 require '../lib/config'
 require 'pry'
 
+class RServer
+
+  def initialize(url)
+    @url = url
+  end
+
+  def share(shared)
+    begin
+      @drbServer = DRb.start_service(@url, shared) # replace localhost with 0.0.0.0 to allow conns from outside
+      @drbServer.thread.join
+    end
+  end
+
+end
+
 shared_hash = {:counter => 0}
 
 def shared_hash.inc(elem)
@@ -10,11 +25,5 @@ def shared_hash.inc(elem)
   self[elem] = self[elem].next
 end
 
-begin
-  drbServer = DRb.start_service(DrbConfig::server_url, shared_hash) # replace localhost with 0.0.0.0 to allow conns from outside
-  drbServer.thread.join
-rescue Errno::EADDRINUSE => e
-  puts e.message << '. Use another port.'
-rescue Exception => e
-  puts 'Another fail: ' << e.message
-end
+remote = RServer.new DrbConfig::server_url
+remote.share(shared_hash)
